f05f60c209adf7bfb5b9f0af97122ee097b651f8
Sandesh Karki <de.sandesh@gmail.com>
2017-05-29 11:15:21 +1000

    api works

--- dlink.js
+++ dlink.js
@@ -7,18 +7,35 @@ $(".btn").click(function () {
   var sTitle = $('.s-title').val();
   var sDesc = $('.s-desc').val();
   var sImg = $('.s-img').val();
-  var option = "SHORT"
+  var option = "SHORT";
 
- var dynamicLink = domain + "?link=" + cLink + "&st=" + sTitle + "&sd=" + sDesc + "&si=" + sImg + "&option=SHORT3";
- console.log(dynamicLink);
+ // var dynamicLink = domain + "?link=" + cLink + "&st=" + sTitle + "&sd=" + sDesc + "&si=" + sImg + "&option=SHORT3";
+ // console.log(dynamicLink);
 
+ $.ajax({
+   type: "POST",
+   url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyAsFPNuGZuYfK042x6OBXpMa4TCSfXE5ds",
+   contentType: "application/json",
+   processData: false,
 
-  $.ajax({
-    type: "POST",
-    url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyAsFPNuGZuYfK042x6OBXpMa4TCSfXE5ds",
-    ContentType: "application/json
+   data:JSON.stringify({
+     "dynamicLinkInfo": {
+     "dynamicLinkDomain": "yx55s.app.goo.gl",
+     "link": cLink,
+
+     "socialMetaTagInfo": {
+      "socialTitle": sTitle,
+      "socialDescription": sDesc,
+      "socialImageLink": sImg
+    }
+  },
+  "suffix": {
+   "option": option
+  }
+   })
+  }).done(function(links){
+
     console.log(links);
-
     $('.response').text(links.shortLink);
   });
 })
--- test.js
+++ test.js
@@ -1,27 +1,37 @@
-console.log('d-link22');
-
-$.ajax({
-  type: "POST",
-  url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyAsFPNuGZuYfK042x6OBXpMa4TCSfXE5ds",
-  contentType: "application/json",
-  processData: false,
-  data:JSON.stringify({
-    "dynamicLinkInfo": {
-    "dynamicLinkDomain": "yx55s.app.goo.gl",
-    "link": "http://www.rewardle.com/termsconditions"
-    }
-  })
-}).done(function(links) {
-  console.log(links);
-});
-
-
-//works
-
-//munal social links
- // "https://yx55s.app.goo.gl/?link=http://www.rewardle.com/business&st=123Test&sd=social_media_test&si=https://media.cdn.gradconnection.com/uploads/5682b86e-59bb-447b-b6a3-3729d569eec4-Rewardle-Banner.png"
-
-//longDynamicLink
-//  data: {
-  //     "longDynamicLink": "https://yx55s.app.goo.gl/?link=http://www.rewardle.com/business"
-// }
+// console.log('d-link22');
+//
+// $.ajax({
+//   type: "POST",
+//   url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=AIzaSyAsFPNuGZuYfK042x6OBXpMa4TCSfXE5ds",
+//   contentType: "application/json",
+//   processData: false,
+//
+//   data:JSON.stringify({
+//     "dynamicLinkInfo": {
+//     "dynamicLinkDomain": "yx55s.app.goo.gl",
+//     "link": "http://www.rewardle.com/termsconditions",
+//
+//     "socialMetaTagInfo": {
+//      "socialTitle": "test2",
+//      "socialDescription": "Rewadle Test2",
+//      "socialImageLink": "http://m.img.brothersoft.com/android/635/1352510745_icon.png"
+//    }
+//  },
+//  "suffix": {
+//   "option": "SHORT"
+//  }
+//   })
+// }).done(function(links) {
+//   console.log(links);
+// });
+//
+//
+// //works
+//
+// //munal social links
+//  // "https://yx55s.app.goo.gl/?link=http://www.rewardle.com/business&st=123Test&sd=social_media_test&si=https://media.cdn.gradconnection.com/uploads/5682b86e-59bb-447b-b6a3-3729d569eec4-Rewardle-Banner.png"
+//
+// //longDynamicLink
+// //  data: {
+//   //     "longDynamicLink": "https://yx55s.app.goo.gl/?link=http://www.rewardle.com/business"
+// // }
