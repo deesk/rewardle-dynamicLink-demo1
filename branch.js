e17bd99aec49e8701267a4312a9b381e4a1274c3
Sandesh Karki <de.sandesh@gmail.com>
2017-05-29 19:49:11 +1000

    git ignore updated

--- branch.js
+++ branch.js
@@ -1,18 +1,18 @@
 console.log('branch');
-
-$.ajax({
-
-  type: "POST",
-  url: "https://api.branch.io/",
-  contentType: "application/json"
-
-
-
-
-}).done(function(data){
-    console.log(data);
-
-})
-
--d '{"branch_key":"key_live_feebgAAhbH9Tv85H5wLQhpdaefiZv5Dv", "campaign":"new_product_annoucement", "channel":"email", "tags":["monday", "test123"], "data":"{\"name\": \"devon\", \"email\": \"devon@branch.io\", \"user_id\": \"12346\", \"$deeplink_path\": \"article/jan/123\", \"$desktop_url\": \"https://branch.io\"}"}' \
-\
+//
+// $.ajax({
+//
+//   type: "POST",
+//   url: "https://api.branch.io/",
+//   contentType: "application/json"
+//
+//
+//
+//
+// }).done(function(data){
+//     console.log(data);
+//
+// })
+//
+// -d '{"branch_key":"key_live_feebgAAhbH9Tv85H5wLQhpdaefiZv5Dv", "campaign":"new_product_annoucement", "channel":"email", "tags":["monday", "test123"], "data":"{\"name\": \"devon\", \"email\": \"devon@branch.io\", \"user_id\": \"12346\", \"$deeplink_path\": \"article/jan/123\", \"$desktop_url\": \"https://branch.io\"}"}' \
+// \
--- dlink.js
+++ dlink.js
@@ -2,38 +2,36 @@ console.log('d-link');
 
 $(".btn").click(function () {
   var domain = "https://yx55s.app.goo.gl/";
-  var cLink = $('.c-link').val(); //content link
-  var sTitle = $('.s-title').val(); // social media title
-  var sDesc = $('.s-desc').val(); // social media description
-  var sImg = $('.s-img').val(); // social media image
-  var option = "SHORT"; // suffix (length of link)
+  //content link
+  var cLink = $('.c-link').val();
+  // social media title
+  var sTitle = $('.s-title').val();
+  // social media description
+  var sDesc = $('.s-desc').val();
+  // social media image
+  var sImg = $('.s-img').val();
+  // suffix (length of link)
+  var option = "SHORT";
 
- $.ajax({
-   type: "POST",
-   url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + api_key,
-   contentType: "application/json",
-   processData: false,
-
+  $.ajax({
+    type: "POST",
+    url: "https://firebasedynamiclinks.googleapis.com/v1/shortLinks?key=" + api_key,
+    contentType: "application/json",
+    processData: false,
     data:JSON.stringify({
       "dynamicLinkInfo": {
-      "dynamicLinkDomain": "yx55s.app.goo.gl",
-      "link": cLink,
-        "socialMetaTagInfo": {
-          "socialTitle": sTitle,
-          "socialDescription": sDesc,
-          "socialImageLink": sImg
+        "dynamicLinkDomain": "yx55s.app.goo.gl",
+        "link": cLink,
+          "socialMetaTagInfo": {
+            "socialTitle": sTitle,
+            "socialDescription": sDesc,
+            "socialImageLink": sImg
+          }
         },
-        "androidInfo": {
-          "androidPackageName": "com.example.android"
-        },
-        "iosInfo": {
-          "iosBundleId": "com.example.ios"
+        "suffix": {
+          "option": option
         }
-      },
-      "suffix": {
-        "option": option
-      }
-    })
+      })
   }).done(function(links){
     console.log(links);
     $('.response').text(links.shortLink);
