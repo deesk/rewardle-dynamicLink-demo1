283bf1a992e7fcb59653cfa935975786133aaeb6
Sandesh Karki <de.sandesh@gmail.com>
2017-05-30 14:35:19 +1000

    branch.js alias type working

--- branch.js
+++ branch.js
@@ -7,11 +7,19 @@ $.ajax({
   processData: false,
   data:JSON.stringify({
   	"branch_key": b_key,
-  	"campaign": "Rewardle | ajax: test0501",
+    "alias" : "alias test",
+  	"campaign": "ajax: test0502 | with alias",
   	"channel" : "facebook",
-  	"data" : "{ \"$og_title\": \"deep linking\" }"
+  	"data" : {
+      "$og_title": "deep linking",
+          }
   })
 }).done(function(data){
     console.log(data);
 
 })
+
+//
+// \"$og_description\": \" description for social media\",
+// \"$og_image_url\": \"https://amazingslider.com/wp-content/uploads/2012/12/dandelion.jpg \",
+// \"$desktop_url\": \"http://blog.rewardle.com/\"
