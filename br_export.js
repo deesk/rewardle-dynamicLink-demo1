a3a85d4e53cdcc1a3eebac9553880a94f3b46ee6
Sandesh Karki <de.sandesh@gmail.com>
2017-05-30 21:40:39 +1000

    branch.js csv file export api..done

---
+++ br_export.js
@@ -0,0 +1,15 @@
+console.log("br export");
+
+var eDate = "2017-05-30";
+
+$.ajax ({
+  url: "https://api.branch.io/v2/export/" + b_key,
+
+  data: {
+    branch_secret : b_secret,
+    export_date : eDate
+  }
+
+}).done(function (data){
+  console.log(data)
+})
